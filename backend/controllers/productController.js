1d0a934c125f80f8e8e64b5f6f761feef7a5036a
Arthath Agary <arthathagary@gmail.com>
2023-08-20 14:52:30 +0530

    add get single product controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,12 @@
 const Product = require('../models/productModel')
 
-const getAllProducts = (req,res,next)=>{
-    res.status(200).json({success:true})
+const getAllProducts =async (req,res,next)=>{
+    const products = await Product.find({});
+    res.status(200).json({
+        success:true,
+        count : products.length,
+        products
+    })
 }
 
 const createProduct = async (req,res,next)=>{
@@ -12,4 +17,18 @@ const createProduct = async (req,res,next)=>{
    })
 }
 
-module.exports = {getAllProducts,createProduct}
+const getSingleProduct = async (req,res,next)=>{
+    const product = await Product.findById(req.params.id);
+    if(!product){
+        return res.status(404).json({
+            success:false,
+            message:"Product Not Found"
+        })
+    }
+    res.status(201).json({
+        success : true,
+        product
+    })
+}
+
+module.exports = {getAllProducts,createProduct,getSingleProduct}
